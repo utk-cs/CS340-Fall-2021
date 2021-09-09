112f2483db12b5e55349dc9bf1305b6737e5b85f
Ben Klein <robobenklein@gmail.com>
2021-09-08 23:49:22 -0400

    teams automation

---
+++ teams.py
@@ -0,0 +1,28 @@
+
+from pathlib import Path
+import roboyml
+import re
+
+teamdir = Path("teams")
+teamfile = Path("teams.yml")
+studentfile = Path("students.yml")
+
+with roboyml.open(studentfile) as students, roboyml.open(teamfile) as teams:
+    print(f"Loaded {len(students)} student entries")
+
+    for teammd in teamdir.glob('*.md'):
+        teamname = str(teammd).split('.')[0].split('/')[1]
+        with teammd.open('r') as f:
+            teamdata = f.read()
+        teams[teamname] = {
+            "name": teamname,
+            "members": {},
+        }
+        for netid in students.keys():
+            if netid in teamdata:
+                teams[teamname]["members"][netid] = {
+                    "netid": students[netid]["netid"],
+                    "github": students[netid]["github"],
+                    "name": students[netid]["name"],
+                }
+                students[netid]["team"] = teamname
