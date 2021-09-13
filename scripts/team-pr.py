f36195a61390e92950a5768395413b54e4285c85
Ben Klein <robobenklein@gmail.com>
2021-09-13 12:23:28 -0400

    handle multiple assignments

--- scripts/team-pr.py
+++ scripts/team-pr.py
@@ -25,7 +25,12 @@ with roboyml.open(studentfile) as students, roboyml.open(teamfile) as teams:
         # print(f"{student['name']} is assigned to {assigned_pulls}")
         team_pulls = [p for p in assigned_pulls if team_label in p.labels and not (p.state == "closed" and p.merged == False)]
         if len(team_pulls) > 1:
-            raise ValueError(f"{student['github']} is assigned to more than one team PR: {[p.number for p in team_pulls]}")
+            print(f"ERROR: {student['github']} is assigned to more than one team PR: {[p.number for p in team_pulls]}")
+            students[netid]['team_pr'] = {
+                "pr": [p.number for p in team_pulls],
+                "state": "DUPLICATE",
+                "merged": "Cannot merge, invalid team PR",
+            }
         elif len(team_pulls) == 0:
             students[netid]['team_pr'] = None
         else:
